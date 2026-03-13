a04b5d9534d3e2387573497da8439449c8d2bb73
shilpakevala08 <148066563+shilpakevala08@users.noreply.github.com>
2026-03-13 13:43:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Pipeline");
         System.out.println("Hi from Jenkins Pipeline");
+        System.out.println("Hi from Jenkins");
     }
 }
